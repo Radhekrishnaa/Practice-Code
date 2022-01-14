f6e4d4b2b6ad76c1afe9dcd46cef29d9b52af60b
RadhekrishnaMishra31 <92598781+Radhekrishnaa@users.noreply.github.com>
2022-01-14 23:36:47 +0530

    Update sum.h

--- header_file_created_by_me/sum.h
+++ header_file_created_by_me/sum.h
@@ -5,3 +5,4 @@ void sum(int a1,int a2){
 }
 // also to this header file work you have to save its copy in the location where all the header files are saved
 //in the memory where our mingw-w64 is stored
+//IN my case my mingw is located at the location:
