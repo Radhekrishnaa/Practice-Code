aaf617d313dd60deb1f7403f9fcc2796edf97f4f
RadhekrishnaMishra31 <92598781+Radhekrishnaa@users.noreply.github.com>
2022-01-14 23:36:11 +0530

    Update sum.h

--- header_file_created_by_me/sum.h
+++ header_file_created_by_me/sum.h
@@ -3,5 +3,5 @@ void sum(int a1,int a2){
  printf("%d",a2);
  printf("%d",a1+a2);
 }
-// also to work this header file you have to save its copy in the location where all the header files are saved
-//in the memory like in the bin or somew
+// also to this header file work you have to save its copy in the location where all the header files are saved
+//in the memory where our mingw-w64 is stored
