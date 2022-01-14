30c582002ff80b15ff7938dd84f8de58b35fbb16
RadhekrishnaMishra31 <92598781+Radhekrishnaa@users.noreply.github.com>
2022-01-14 23:29:30 +0530

    Update sum.h

--- header_file_created_by_me/sum.h
+++ header_file_created_by_me/sum.h
@@ -3,3 +3,5 @@ void sum(int a1,int a2){
  printf("%d",a2);
  printf("%d",a1+a2);
 }
+// also to work this header file you have to save its copy in the location where all the header files are saved
+//in the memory like in the bin or somew
